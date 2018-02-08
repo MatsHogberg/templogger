bf43883fce12b880ac87a5762ba793a8fcf4a7d7
Mats Högberg <mats.hogberg@outlook.com>
2018-02-08 12:38:07 +0100

    Added secrets

---
+++ secrets.js
@@ -0,0 +1,5 @@
+var secretStuff = {
+    "dbConnectionString":"mongodb://weatheradmin:haga280@weathercluster-shard-00-00-dobbb.mongodb.net:27017/temperature?ssl=true&authSource=admin",
+    "weatherApiKey":"aad118cf783212643c937f267735537c"
+}
+exports.secret = secretStuff;
