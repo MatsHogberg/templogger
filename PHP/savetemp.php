5708965c2e5c947ff18d09040a2606476e115ec9
Mats Högberg <mats.hogberg@outlook.com>
2018-02-09 14:37:59 +0100

    Added secrets.php to ignore, use secrets in script

---
+++ PHP/savetemp.php
@@ -0,0 +1,37 @@
+<?php
+    require "secrets.php";
+
+    $myObj->result = "";
+    $myObj->dbResult = "";
+    if(isset($_GET['key']) && $_GET['key'] == $apiKey)
+    {
+        $location = $_GET['loc'];
+        $temp = $_GET['temp'];
+
+        $myObj->temp=$temp;
+        $myObj->location = $location;
+
+        $db = openDb($dbConnection, $dbUser, $dbPassword, $database);
+        $r = insert($db, $temp, $location);
+        mysqli_close($db);
+        $myObj->result ="OK";
+        $myObj->dbResult = $r;
+        echo json_encode($myObj);
+
+    }else{
+        $myObj->result="Error in key";
+    }
+
+    function insert($db, $t, $l){
+        $sql ="INSERT INTO temperature(temp, location) VALUES($t, '$l')";
+        $result = mysqli_query($db, $sql);
+        if(!$result){
+            return $sql;
+        }
+        return $result;
+    }
+    function openDb($dbConnection, $dbUser, $dbPassword, $database){
+        $connection = mysqli_connect("$dbConnection", "$dbUser", "$dbPassword", "$database");
+        return $connection;
+    }
+?>
